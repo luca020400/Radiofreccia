411662ae04380704b536f694dd9d879cc2bdb11c
Luca Stefani <luca.stefani.ge1@gmail.com>
2020-02-09 21:44:52 +0100

    Reformat code

--- radiofreccia/src/main/java/com/luca020400/radiofreccia/classes/SongInfo.kt
+++ radiofreccia/src/main/java/com/luca020400/radiofreccia/classes/SongInfo.kt
@@ -1,8 +1,6 @@
 package com.luca020400.radiofreccia.classes
 
 import com.google.gson.annotations.SerializedName
-import com.luca020400.radiofreccia.classes.Present
-import com.luca020400.radiofreccia.classes.Show
 
 data class SongInfo(
         @SerializedName("show") val show: Show,
