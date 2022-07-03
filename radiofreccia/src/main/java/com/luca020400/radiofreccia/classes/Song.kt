c7fac8e5eccf6e022955e74deb9384dc8938cd8e
Luca Stefani <luca.stefani.ge1@gmail.com>
2022-07-03 19:32:32 +0200

    Make some fields nullable

--- radiofreccia/src/main/java/com/luca020400/radiofreccia/classes/Song.kt
+++ radiofreccia/src/main/java/com/luca020400/radiofreccia/classes/Song.kt
@@ -5,15 +5,16 @@ import com.squareup.moshi.JsonClass
 
 @JsonClass(generateAdapter = true)
 data class Song(
-    @Json(name = "songInfo") val songInfo: SongInfo
+    @Json(name = "songInfo") val songInfo: SongInfo,
 ) {
     @JsonClass(generateAdapter = true)
     data class SongInfo(
         @Json(name = "show") val show: Show,
-        @Json(name = "present") val present: Present?
+        @Json(name = "present") val present: Present?,
     ) {
         @JsonClass(generateAdapter = true)
         data class Show(
+            @Json(name = "prg_id") var prg_id: String,
             @Json(name = "sch_time_from") val sch_time_from: Int,
             @Json(name = "sch_time_to") val sch_time_to: Int,
             @Json(name = "prg_title") val prg_title: String,
@@ -21,7 +22,6 @@ data class Song(
             @Json(name = "ChaId") val chaId: Int,
             @Json(name = "image170") val image170: String,
             @Json(name = "image400") val image400: String,
-            @Json(name = "ScheduleId") val scheduleId: Int?
         )
 
         @JsonClass(generateAdapter = true)
@@ -31,13 +31,13 @@ data class Song(
             @Json(name = "mus_art_id") val mus_art_id: Int,
             @Json(name = "mus_art_name") val mus_art_name: String,
             @Json(name = "musArtGroupIds") val musArtGroupIds: String?,
-            @Json(name = "mus_sng_id") val mus_sng_id: Int,
+            @Json(name = "mus_sng_id") val mus_sng_id: Int?,
             @Json(name = "mus_sng_title") val mus_sng_title: String,
             @Json(name = "mus_sng_itunesurl") val mus_sng_itunesurl: String,
             @Json(name = "mus_sng_itunesreleasedate") val mus_sng_itunesreleasedate: String,
             @Json(name = "mus_sng_itunesalbumname") val mus_sng_itunesalbumname: String,
             @Json(name = "mus_sng_itunescoverbig") val mus_sng_itunescoverbig: String,
-            @Json(name = "artistMedia") val artistMedia: String?
+            @Json(name = "artistMedia") val artistMedia: String?,
         )
     }
 }
